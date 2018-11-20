51769db684f3e4c6ba8a6e805eaacf2094eeedd9
Darren Henderson <darrenh@ccsj.edu>
2018-11-20 14:35:49 -0600

    Added code for SQL Server LocalDB.

---
+++ KingWilliamHotelTest/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace KingWilliamHotelTest.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomDes> RoomDess { get; set; }
+    }
+}
